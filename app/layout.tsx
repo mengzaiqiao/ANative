e1c287cfc6b8712840497279483497b5fc489f43
Zaiqiao Meng <zaiqiao.meng@gmail.com>
2026-08-23 00:35:56 +0800

    Expand core team profiles

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import "./sections.css";
 import "./redesign.css";
+import "./team.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
